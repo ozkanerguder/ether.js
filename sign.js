9a7c241a131cfd77b892035aaa42c41e3ba92884
Özkan Ergüder <erguderozkan@gmail.com>
2024-01-24 19:21:18 +0300

    sign file updated

---
+++ sign.js
@@ -0,0 +1,13 @@
+const ethers = require('ethers');
+const { Wallet, utils } = ethers;
+const { wallet1 } = require('./wallets');
+
+// TODO: replace all undefined values
+const signaturePromise = wallet1.signTransaction({
+    value: 1,
+    to: "0xdD0DC6FB59E100ee4fA9900c2088053bBe14DE92", 
+    gasLimit: 21000,
+    gasPrice: 2
+});
+
+module.exports = signaturePromise;
